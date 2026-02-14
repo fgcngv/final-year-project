ad19b188599dafa363076a66cf6441ec95047b2f
fgcngv <birhanugezahegn099@gmail.com>
2026-02-14 20:32:31 +0300

    verify page corrected

--- app/[locale]/(protected)/check-out/verify/page.tsx
+++ app/[locale]/(protected)/check-out/verify/page.tsx
@@ -1,35 +1,81 @@
 
 
+// "use client";
+
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+
+// export default function VerifyingPage() {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const searchParams = new URLSearchParams(window.location.search);
+//     const tx_ref = searchParams.get("tx_ref");
+//     if (!tx_ref) {
+//       router.replace("/check-out/failed");
+//       return;
+//     }
+
+//     async function verify() {
+//       const res = await fetch(`/api/chapa/verify?tx_ref=${tx_ref}`);
+//       const data = await res.json();
+
+
+//       console.log("data : ",data)
+
+//       if (data.success) {
+//         router.replace("/check-out/success");
+//       } else {
+//         router.replace("/check-out/failed");
+//       }
+//     }
+
+//     verify();
+//   }, [router]);
+
+//   return <p>Verifying your payment...</p>;
+// }
+
+
+
+
 "use client";
 
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function VerifyingPage() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const tx_ref = searchParams.get("tx_ref");
 
   useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const tx_ref = searchParams.get("tx_ref");
     if (!tx_ref) {
       router.replace("/check-out/failed");
       return;
     }
 
     async function verify() {
-      const res = await fetch(`/api/chapa/verify?tx_ref=${tx_ref}`);
+      const locale = window.location.pathname.split("/")[1];
+
+      const res = await fetch(
+        `/${locale}/api/chapa/verify?tx_ref=${tx_ref}`
+      );
+
       const data = await res.json();
 
+      console.log("data:", data);
+
       if (data.success) {
-        router.replace("/check-out/success");
+        router.replace(`/${locale}/check-out/success`);
       } else {
-        router.replace("/check-out/failed");
+        router.replace(`/${locale}/check-out/failed`);
       }
     }
 
     verify();
-  }, [router]);
+  }, [router, tx_ref]);
 
   return <p>Verifying your payment...</p>;
 }
--- app/[locale]/api/chapa/verify/route.ts
+++ app/[locale]/api/chapa/verify/route.ts
@@ -90,3 +90,6 @@ export async function GET(req: Request) {
 
   return new Response(JSON.stringify({ success: true }));
 }
+
+
+
