6eeb18330dbd021da0faeae13b8f6c8904d93031
fgcngv <birhanugezahegn099@gmail.com>
2026-02-21 22:53:59 +0300

    erorr gone

--- app/[locale]/theme/page.tsx
+++ app/[locale]/theme/page.tsx
@@ -4,9 +4,9 @@ import { useTheme } from "@/components/checkTheme";
 import { Button } from "@/components/ui/button";
 
 export default function SomeComponent() {
-  const { theme, toggleTheme } = useTheme();
+  const { language, toggleLanguage } = useTheme();
 
   return (
-      <Button onClick={toggleTheme} className="cursor-pointer font-bold bg-green-600 hover:bg-green-700 active:bg-green-800">{theme}</Button>
+      <Button onClick={toggleLanguage} className="cursor-pointer font-bold bg-green-600 hover:bg-green-700 active:bg-green-800">{language}</Button>
   );
 }
