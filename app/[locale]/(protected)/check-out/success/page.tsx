1e7af09e8d4664dbeec0af0a50224c96631afdc4
fgcngv <birhanugezahegn099@gmail.com>
2026-04-13 09:58:40 +0300

    a

--- app/[locale]/(protected)/check-out/success/page.tsx
+++ app/[locale]/(protected)/check-out/success/page.tsx
@@ -1,15 +1,43 @@
+// export const dynamic = "force-dynamic";
+
+
+// import OrderConfirmation from "@/components/checkout/OrderConfirmation";
+
+
+// function PaymentSuccess() {
+//     return ( 
+//         <div>
+//             <OrderConfirmation orderId="cmlewco1j001h0kuporfbrjjh" />
+//         </div>
+//      );
+// }
+
+// export default PaymentSuccess;
+
+
+
+
 export const dynamic = "force-dynamic";
 
-
 import OrderConfirmation from "@/components/checkout/OrderConfirmation";
+import { getOrdersByPaymentId } from "@/utils/services/order";
 
+export default async function PaymentSuccess({
+  params,
+}: {
+  params: Promise<{ paymentId: string }>;
+}) {
+  const { paymentId } = await params;
 
-function PaymentSuccess() {
-    return ( 
-        <div>
-            <OrderConfirmation orderId="cmlewco1j001h0kuporfbrjjh" />
-        </div>
-     );
+  const result = await getOrdersByPaymentId(paymentId);
+
+  if (!result.success || !result.data) {
+    return (
+      <div className="p-10 text-center text-red-500">
+        Failed to load order confirmation.
+      </div>
+    );
+  }
+
+  return <OrderConfirmation orders={result.data} paymentId={paymentId} />;
 }
-
-export default PaymentSuccess;
